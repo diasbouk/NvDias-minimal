0335da429efe991757d5c7fb5a9a3701cd059083
diasbouk <boukdsaid@gmail.com>
2024-09-29 12:09:16 +0100

    New update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -16,14 +16,13 @@ end
 --[[ Keymappings - shortcuts ]]
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<space>gb", ":Neotree toggle<CR>")
 vim.keymap.set("t", "<C-w>w", "<cmd><C-w>w<CR>")
 vim.keymap.set("n", "<space>h", ":lua OpenBottomTerminal()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<space>v", ":lua OpenSideTerminal()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
 vim.cmd("nnoremap <space>ca <cmd>lua vim.lsp.buf.code_action()<CR>")
 vim.keymap.set("n", "<C-s>", ":wa<CR>")
--- vim.keymap.set("n", "<space>gb", ":Ex<CR>")
+vim.keymap.set("n", "<space>gb", ":Ex<CR>")
 vim.cmd("tnoremap <C-w> <C-\\><C-N><C-w>")
 vim.cmd("tnoremap <A-j> <C-\\><C-N><C-w>j")
 vim.cmd("tnoremap <C-k> <C-\\><C-N><C-w>k")
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,15 +1,15 @@
 return {
-	{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+	{ "folke/tokyonight.nvim",     lazy = false,     priority = 1000, opts = {} },
 	{ "frenzyexists/aquarium-vim" },
-	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false,    priority = 1000 },
 	{ "AlessandroYorba/Alduin" },
-
+	{ "ellisonleao/gruvbox.nvim",  priority = 1000,  config = true,   opts = ... },
+	{ "rebelot/kanagawa.nvim" },
 	{
-		'echasnovski/mini.nvim',
-		version = '*' ,
+		"echasnovski/mini.nvim",
+		version = "*",
 		config = function()
-			require('mini.statusline').setup()
+			require("mini.statusline").setup()
 		end,
 	},
-	{'rebelot/kanagawa.nvim'},
 }
